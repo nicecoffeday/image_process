6bf52a36edbde13556ad1931e70af455ac292e65
nicecoffeday <71484908+nicecoffeday@users.noreply.github.com>
2021-09-05 20:59:48 +0800

    Add files via upload
    add listdir to check file's name in the folder

--- greystep.py
+++ greystep.py
@@ -1,4 +1,10 @@
 from PIL import Image
+import os
+
 image_file = Image.open("pumpkin.png") # open colour image
 image_file = image_file.convert('L') # convert image to black and white
 image_file.save('result.png')
+		
+
+for file in os.listdir('.'): #列出所在位置中 檔案的檔名
+	print(file)
