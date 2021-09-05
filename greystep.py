a5f1ebc37b40acedadd08bae740ca4d502b00598
nicecoffeday <71484908+nicecoffeday@users.noreply.github.com>
2021-09-05 21:47:14 +0800

    更新
    add path concept

--- greystep.py
+++ greystep.py
@@ -1,10 +1,10 @@
 from PIL import Image
 import os
 
-image_file = Image.open("pumpkin.png") # open colour image
-image_file = image_file.convert('L') # convert image to black and white
-image_file.save('result.png')
-		
 
-for file in os.listdir('.'): #列出所在位置中 檔案的檔名
-	print(file)
+for file in os.listdir('orig'): #列出所在位置中 檔案的檔名
+    if file.endswith('.jpg'):
+        image_file = Image.open('orig/' + file) # open colour image, add path orig/
+        image_file = image_file.convert('L') # convert image to black and white
+        image_file.save('result/' + file[:-4] + '_grey.png')
+
