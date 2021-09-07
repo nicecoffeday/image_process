d3ddd38435d42d936eabe0feb8c9eae4b0eb7f66
nicecoffeday <71484908+nicecoffeday@users.noreply.github.com>
2021-09-07 15:07:29 +0800

    use path
    use path to sort file  'os.path.join'

--- greystep.py
+++ greystep.py
@@ -4,7 +4,6 @@ import os
 
 for file in os.listdir('orig'): #列出所在位置中 檔案的檔名
     if file.endswith('.jpg'):
-        image_file = Image.open('orig/' + file) # open colour image, add path orig/
+        image_file = Image.open(os.path.join('orig', file)) # open colour image, add path orig/
         image_file = image_file.convert('L') # convert image to black and white
-        image_file.save('result/' + file[:-4] + '_grey.png')
-
+        image_file.save(os.path.join('result', file[:-4] + '_grey.png'))
